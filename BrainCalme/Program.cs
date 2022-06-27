385432622d30450737017f2d42852f9417a4490e
Mohamadreza Nakhleh <neo.vortex@pm.me>
2022-06-27 16:03:56 +0430

    update readme

--- BrainCalme/Program.cs
+++ BrainCalme/Program.cs
@@ -9,94 +9,94 @@
         {
             var pointerPools = stackalloc byte[30000];
                   while (programm_index <= input.Length -1 )
-                {
-                    var token = input[programm_index];
-                    switch (token)
-                    {
-                        case '>':
-                            while (input[programm_index] == '>')
-                            {
-                                _pointerIndex++;
-                                programm_index++;
-                            }
-                            programm_index--;
-                            break;
-                        case '<':
-                            while (input[programm_index] == '<')
-                            {
-                                _pointerIndex--;
-                                programm_index++;
-                            }
-                            programm_index--;
+                  {
+                      var token = input[programm_index];
+                      switch (token)
+                      {
+                          case '>':
+                              while (input[programm_index] == '>')
+                              {
+                                  _pointerIndex++;
+                                  programm_index++;
+                              }
+                              programm_index--;
+                              break;
+                          case '<':
+                              while (input[programm_index] == '<')
+                              {
+                                  _pointerIndex--;
+                                  programm_index++;
+                              }
+                              programm_index--;
 
-                            break;
-                        case '+':
-                            while (input[programm_index] == '+')
-                            {
-                                pointerPools[_pointerIndex]++;
-                                programm_index++;
-                            }
-                            programm_index--;
-                            break;
-                        case '-':
-                            while (input[programm_index] == '-')
-                            {
-                                pointerPools[_pointerIndex]--;
-                                programm_index++;
-                            }
-                            programm_index--;
-                            break;
-                        case '.' :
-                            Console.Write(pointerPools[_pointerIndex] == 10 ? Environment.NewLine : Convert.ToChar(pointerPools[_pointerIndex])  );
-                            break;
-                        case ',':
-                            Console.WriteLine("[Interpreter : Enter input :]");
-                            pointerPools[_pointerIndex] = (byte)(int.Parse(Console.ReadLine() ?? throw new Exception("Bad input")));
-                            break;
-                        case '[':
-                            if (pointerPools[_pointerIndex] == 0)
-                            {
-                                var helper = 0;
-                                programm_index++;
-                                while (input[programm_index] != ']' || helper > 0 )
-                                {
-                                    if (input[programm_index] == '[')
-                                    {
-                                        helper++;
-                                    }
-                                    if (input[programm_index] == ']')
-                                    {
-                                        helper--;
-                                    }
-                                    programm_index++;
-                                }  
-                            }
-                            break;
+                              break;
+                          case '+':
+                              while (input[programm_index] == '+')
+                              {
+                                  pointerPools[_pointerIndex]++;
+                                  programm_index++;
+                              }
+                              programm_index--;
+                              break;
+                          case '-':
+                              while (input[programm_index] == '-')
+                              {
+                                  pointerPools[_pointerIndex]--;
+                                  programm_index++;
+                              }
+                              programm_index--;
+                              break;
+                          case '.' :
+                              Console.Write(pointerPools[_pointerIndex] == 10 ? Environment.NewLine : Convert.ToChar(pointerPools[_pointerIndex])  );
+                              break;
+                          case ',':
+                              Console.WriteLine("[Interpreter : Enter input :]");
+                              pointerPools[_pointerIndex] = (byte)(int.Parse(Console.ReadLine() ?? throw new Exception("Bad input")));
+                              break;
+                          case '[':
+                              if (pointerPools[_pointerIndex] == 0)
+                              {
+                                  var helper = 0;
+                                  programm_index++;
+                                  while (input[programm_index] != ']' || helper > 0 )
+                                  {
+                                      if (input[programm_index] == '[')
+                                      {
+                                          helper++;
+                                      }
+                                      if (input[programm_index] == ']')
+                                      {
+                                          helper--;
+                                      }
+                                      programm_index++;
+                                  }  
+                              }
+                              break;
                         
-                        case ']' :
-                            if (pointerPools[_pointerIndex] != 0)
-                            {
-                                var helper2 = 0;
-                                programm_index--;
-                                while (input[programm_index] != '[' || helper2 > 0 )
-                                {
-                                    if (input[programm_index] == ']')
-                                    {
-                                        helper2++;
-                                    }
-                                    if (input[programm_index] == '[')
-                                    {
-                                        helper2--;
-                                    }
-                                    programm_index--;
-                                }  
-                                programm_index--;
-                            }
+                          case ']' :
+                              if (pointerPools[_pointerIndex] != 0)
+                              {
+                                  var helper2 = 0;
+                                  programm_index--;
+                                  while (input[programm_index] != '[' || helper2 > 0 )
+                                  {
+                                      if (input[programm_index] == ']')
+                                      {
+                                          helper2++;
+                                      }
+                                      if (input[programm_index] == '[')
+                                      {
+                                          helper2--;
+                                      }
+                                      programm_index--;
+                                  }  
+                                  programm_index--;
+                              }
                            
-                            break;
-                    }
-                    programm_index++;
-                }
+                              break;
+                      }
+                      programm_index++;
+                  }
             
         }
         catch (Exception e)
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # BrainCalme
+
+A very simple Brainfuck interpreter intended for educational purposes with little to no optimization in mind.
